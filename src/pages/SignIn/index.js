d997c4d9bb262bd02ca6860f8203b96e36484524
Luis Geniole <luis.geniole@gmail.com>
2019-11-20 20:43:53 -0300

    Create SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -16,7 +16,7 @@ import {
   SignLinkText,
 } from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   return (
     <Background>
       <Container>
@@ -40,7 +40,7 @@ export default function SignIn() {
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
         </Form>
 
-        <SignLink onPress={() => {}}>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText>Criar conta gratuita</SignLinkText>
         </SignLink>
       </Container>
