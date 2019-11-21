ce7a1c3f37939e09065177b57c8daedd47349234
Luis Geniole <luis.geniole@gmail.com>
2019-11-20 23:02:49 -0300

    add&config Redux&Saga&etc.

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({
-    host: '192.168.0.17',
-  })
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({
+      host: '192.168.0.17',
+    })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
