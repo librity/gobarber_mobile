0a5b288ee216d15af14be62711300bf1892db3ba
Luis Geniole <luis.geniole@gmail.com>
2019-11-22 21:12:48 -0300

    create DateInput

---
+++ src/components/DateInput/index.android.js
@@ -0,0 +1,36 @@
+import React, { useMemo } from 'react';
+import { DatePickerAndroid } from 'react-native';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, DateButton, DateText } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const formattedDate = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+
+  const handleOpenPicker = async () => {
+    const { action, year, month, day } = await DatePickerAndroid.open({
+      mode: 'spinner',
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+
+      onChange(selectedDate);
+    }
+  };
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" color="#fff" size={20} />
+        <DateText>{formattedDate}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
